24f4cdd705193e40aa4796477d8b74ba24abb124
jminac00 <jminac00@estudiantes.unileon.es>
2026-06-05 16:34:17 +0200

    feat(wallet): update redeemPoints API call to accept pointsToRedeem amount

--- frontend/src/lib/wallet.ts
+++ frontend/src/lib/wallet.ts
@@ -2,8 +2,8 @@ import { api } from './api';
 import type { Redemption, PointsHistoryEntry } from '../types';
 
 export const walletApi = {
-    redeemPoints: () =>
-        api.post<{ data: Redemption }>('/users/me/redeem-points', {}).then(r => r.data),
+    redeemPoints: (pointsToRedeem: number) =>
+        api.post<{ data: Redemption }>('/users/me/redeem-points', { points_to_redeem: pointsToRedeem }).then(r => r.data),
     getPointsHistory: () =>
         api.get<{ data: PointsHistoryEntry[] }>('/users/me/points-history').then(r => r.data),
 };
